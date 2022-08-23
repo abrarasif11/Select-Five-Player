6d894b1a68b8429cd34ddaba9141ae79dadfee32
abrarasif11 <faasif929@gmail.com>
2022-08-23 19:48:03 +0600

    last-commit

--- cacl.js
+++ cacl.js
@@ -11,8 +11,8 @@ function addPlayerName(element){
     const listSize = document.getElementById('player-list').childNodes;
     const numberofChilds = listSize.length;
     if(numberofChilds > 6){
-        alert("Your Limit is Over. Already Select 5 Players");
-        newPlayer.style.listStyle ='none'
+        alert("Your Limit is Over. Already Select five Players");
+        newPlayer.style.listStyle ='none';
         newPlayer.innerText = ' ';
     }
     onclick = element.disabled = "true";
@@ -27,9 +27,48 @@ document.getElementById('btn-calculate').addEventListener('click', function(){
     const perPlayerFieldString = perPlayerField.value;
     const previousPerPlayer = parseInt(perPlayerFieldString);
 
-    const expencesTotal = document.getElementById('expences-total')
-
-    const totalPerPlayer = (numberofChilds - 2) * previousPerPlayer;
+    const expencesTotal = document.getElementById('expences-total');
+   
+    let totalPerPlayer ;
+    if(numberofChilds <= 6){
+       totalPerPlayer = (numberofChilds - 1) * previousPerPlayer;
+    }
+    else if(numberofChilds > 6){
+       totalPerPlayer = 5 * previousPerPlayer;
+    }
+    
     expencesTotal.innerText = totalPerPlayer;
 });
 
+// *********** Total Cost ********** //
+document.getElementById('calc-total-btn').addEventListener('click', function(){
+     
+    const managerField = document.getElementById('manager-field');
+    const managerFieldString = managerField.value;
+    const totalManegerFee = parseInt(managerFieldString);
+
+    const coachField = document.getElementById('coach-field');
+    const coachFieldString = coachField.value;
+    const totalCoachFee = parseInt(coachFieldString);
+
+    const perPlayerField = document.getElementById('salary-field');
+    const perPlayerFieldString = perPlayerField.value;
+    const previousPerPlayer = parseInt(perPlayerFieldString);
+
+    const listSize = document.getElementById('player-list').childNodes;
+    const numberofChilds = listSize.length; 
+
+    let totalPerPlayer ;
+    if(numberofChilds <= 6){
+       totalPerPlayer = (numberofChilds - 1) * previousPerPlayer;
+    }
+    else if(numberofChilds > 6){
+       totalPerPlayer = 5 * previousPerPlayer;
+    }
+
+    const totalFee = document.getElementById('total-fee');
+
+
+    const TotalAmountFee = totalPerPlayer + totalManegerFee + totalCoachFee;
+    totalFee.innerText = TotalAmountFee;
+})
